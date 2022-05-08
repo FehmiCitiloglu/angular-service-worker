2d90b0c0a828885a10371976515b3f5e46f0ac40
FehmiCitiloglu <fehmicitiloglu@gmail.com>
2022-05-08 22:55:04 +0300

    last commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
-import { Post } from './post.model';
+import { Post } from "./post.model";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
   posts: Post[] = [];
@@ -15,7 +15,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.http
-      .get<Post[]>('https://jsonplaceholder.typicode.com/posts')
-      .subscribe(fetchedPosts => (this.posts = fetchedPosts));
+      .get<Post[]>("https://jsonplaceholder.typicode.com/posts")
+      .subscribe((fetchedPosts) => (this.posts = fetchedPosts));
   }
 }
